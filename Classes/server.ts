88101c02f7e8138052f159ec86b379dcd383ea25
MoeO30 <ufosmxl@gmail.com>
2022-01-19 09:22:18 -0800

    Seccion 4

--- Classes/server.ts
+++ Classes/server.ts
@@ -1,20 +1,49 @@
 
 import express from 'express';
 import { SERVER_PORT } from '../global/envarioment';
-
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/sockets';
 
 export default class Server {
+private static _instance: Server; // ayuda a tener una unica instaicia de la clase server y un socket
 public  app: express.Application;
 public port: number;
+public io: socketIO.Server;
+private httpServer: http.Server;
 
 
-constructor() {
+
+private constructor() {
     this.app = express();
     this.port = SERVER_PORT;
+    this.httpServer = new http.Server( this.app );
+    // this.io =  socketIO(this.httpServer); /// provoca error http request origin access que hace referencia a los permisos 
+    this.io = new socketIO.Server( this.httpServer, { cors: { origin: true, credentials: true } } );
+
+    this.escucharSockets();
 }
 
-start(callback: any):void {
-this.app.listen(this.port, callback);
+public static get instance() {
+return  this._instance || (this._instance =  new this());
 }
+
+            private escucharSockets(){
+            console.log('escuchando conexion de sockets');
+                this.io.on('connection', cliente =>{
+                console.log('Nuevo Cliente Conectado');
+
+                socket.mensaje(cliente, this.io);
+                socket.desconectar(cliente); /// llama a la funcion creada en la carpeta sockets
+                // cliente.on('disconnect', () { 
+                // console.log('Cliente desconectado');
+                // });
+                });
+            }
+
+            start(callback: any):void {
+            // this.app.listen(this.port, callback);
+            this.httpServer.listen(this.port, callback);
+            }
 
 }
--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import router  from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server =  new Server();
+const server =  Server.instance;
 //BodyPARSEr
 server.app.use(bodyParser.urlencoded({extended:true}));
 server.app.use(bodyParser.json());
@@ -18,5 +18,6 @@ server.app.use(cors({origin:true, credentials:true}))
 server.app.use('/', router)
 
 server.start(()=>{
-    console.log(`Servidor corriendo en el puerto ${server.port}`)
+    console.log(`Servidor corriendo en el puerto ${server.port}`);
+    
 });
